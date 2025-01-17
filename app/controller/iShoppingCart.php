2dea5a1ceb2752c06f1a9f9f39b95d731c0629c0
Kevin González <keving5726@gmail.com>
2025-01-16 21:02:53 -0400

    Add iShoppingCart interface

---
+++ app/controller/iShoppingCart.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+interface iShoppingCart
+{
+    public function index();
+    public function add();
+    public function remove();
+    public function clear();
+}
