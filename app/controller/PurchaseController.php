10b4882378fc8515e37b895bc8577178cef902f2
Kevin González <keving5726@gmail.com>
2025-02-03 20:29:15 -0400

    Update PurchaseController controller

--- app/controller/PurchaseController.php
+++ app/controller/PurchaseController.php
@@ -8,9 +8,9 @@ use App\Core\Session;
 
 class PurchaseController implements iController
 {
-    private ?int $balance;
+    private ?float $balance;
     private ?array $purchases;
-    private ?int $total;
+    private ?float $total;
     private ?array $alert;
 
     public function __construct()
@@ -28,73 +28,7 @@ class PurchaseController implements iController
 
     public function create()
     {
-        if (empty($_POST["shipping"]))
-        {
-            $this->alert = [
-                'message' => "Please select a shipping option",
-                'type' => "warning",
-            ];
-
-            header('Location: /shoppingcart');
-            return;
-        }
-        elseif($_POST["shipping"] === "option1")
-        {
-            $this->purchases(0);
-            return;
-        }
-        elseif($_POST["shipping"] === "option2")
-        {
-            $this->purchases(5);
-            return;
-        }
-        else
-        {
-            $this->alert = [
-                'message' => "Invalid shipping option",
-                'type' => "warning",
-            ];
-
-            header('Location: /shoppingcart');
-            return;
-        }
-    }
-
-    public function show(string $code)
-    {
-        foreach ($_SESSION["purchases"] as $value)
-        {
-            if ($value["code"] === $code)
-            {
-                foreach ($value["purchase"] as $value)
-                {
-                    $object = new \stdClass;
-                    $object->code = $value->code;
-                    $object->name = $value->name;
-                    $object->price = $value->price;
-                    $object->quantity = $value->quantity;
-                    $object->total = $value->total;
-
-                    $purchase[] = $object;
-                }
-
-                return View::show("purchases.show", ['purchase' => $purchase, 'title' => 'Purchase Details']);
-               
-            }
-        }
-    }
-
-    public function edit()
-    {
-    }
-
-    public function delete()
-    {
-    }
-
-    public function purchases(int $shipping): void
-    {
-        if ($this->balance < ($_SESSION["total"] + $shipping))
+        if ($this->balance < $_SESSION["total"])
         {
             $this->alert = [
                 'message' => "Your balance is insufficient",
@@ -111,24 +45,66 @@ class PurchaseController implements iController
             $object->code = $value->code;
             $object->name = $value->name;
             $object->price = $value->price;
-            $object->quantity = $value->quantity;
-            $object->total = $value->total;
+            $object->items = $value->items;
+            $object->subtotal = $value->subtotal;
 
             $purchase[] = $object;
         }
 
-        ($shipping === 0) ? $shipping_option = "Pick Up (USD 0)" : $shipping_option = "UPS (USD 5)";
         $code = rand();
-        $this->total += ($_SESSION["total"] + $shipping);
-        $this->purchases[] = ["code" => "$code", "purchase" => $purchase, "date" => (new \DateTime())->format('Y-m-d H:i:s'), "shipping" => $shipping_option, "total" => ($_SESSION["total"] + $shipping)];
+        $this->total += $_SESSION["total"];
+        $this->purchases[] = [
+            "code" => "$code",
+            "date" => (new \DateTime())->format('Y-m-d H:i:s'),
+            "items" => $_SESSION["items"],
+            "subtotal" => $_SESSION["subtotal"],
+            "discount" => $_SESSION["discount"],
+            "shippingOption" => $_SESSION["shippingOption"],
+            "totalBeforeTax" => $_SESSION["totalBeforeTax"],
+            "tax" => $_SESSION["tax"],
+            "total" => $_SESSION["total"],
+            "purchase" => $purchase
+        ];
 
-        $this->balance -= ($_SESSION["total"] + $shipping);
-        Session::clean();
+        $this->balance -= $_SESSION["total"];
+        Session::clear();
         $this->alert = [
             'message' => "Your purchase have been added successfully",
             'type' => "success",
         ];
-        header('Location: /shoppingcart');
+        header('Location: /purchases');
         return;
+    }
+
+    public function show(string $code)
+    {
+        foreach ($_SESSION["purchases"] as $value)
+        {
+            if ($value["code"] === $code)
+            {
+                foreach ($value["purchase"] as $value)
+                {
+                    $object = new \stdClass;
+                    $object->code = $value->code;
+                    $object->name = $value->name;
+                    $object->price = $value->price;
+                    $object->items = $value->items;
+                    $object->subtotal = $value->subtotal;
+
+                    $purchase[] = $object;
+                }
+
+                return View::show("purchases.show", ['purchase' => $purchase, 'title' => 'Purchase Details']);
+               
+            }
+        }
+    }
+
+    public function edit()
+    {
+    }
+
+    public function delete()
+    {
     }
 }
