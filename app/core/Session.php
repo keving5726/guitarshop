b714c148eb030390aec2d49400b3f79a02e33201
Kevin González <keving5726@gmail.com>
2025-04-05 22:07:46 -0400

    Update session variables

--- app/core/Session.php
+++ app/core/Session.php
@@ -7,7 +7,7 @@ class Session
 {
     public function __construct()
     {
-        if (session_status() !== 2)
+        if (session_status() !== PHP_SESSION_ACTIVE)
         {
             session_start();
 
@@ -15,18 +15,48 @@ class Session
             {
                 $_SESSION["balance"] = (float) getenv('BALANCE') ?: '5000';
             }
+
+            if (!isset($_SESSION["items"]))
+            {
+                $_SESSION["items"] = "0";
+            }
+
+            if (!isset($_SESSION["subtotal"]))
+            {
+                $_SESSION["subtotal"] = "0";
+            }
+
+            if (!isset($_SESSION["discount"]))
+            {
+                $_SESSION["discount"] = "0";
+            }
+
+            if (!isset($_SESSION["totalBeforeTax"]))
+            {
+                $_SESSION["totalBeforeTax"] = "0";
+            }
+
+            if (!isset($_SESSION["tax"]))
+            {
+                $_SESSION["tax"] = "0";
+            }
+
+            if (!isset($_SESSION["total"]))
+            {
+                $_SESSION["total"] = "0";
+            }
         }
     }
 
     public static function clear(): void
     {
         $_SESSION["cart"] = NULL;
-        $_SESSION["items"] = NULL;
-        $_SESSION["subtotal"] = NULL;
-        $_SESSION["discount"] = NULL;
+        $_SESSION["items"] = "0";
+        $_SESSION["subtotal"] = "0";
+        $_SESSION["discount"] = "0";
         $_SESSION["shippingOption"] = NULL;
-        $_SESSION["totalBeforeTax"] = NULL;
-        $_SESSION["tax"] = NULL;
-        $_SESSION["total"] = NULL;
+        $_SESSION["totalBeforeTax"] = "0";
+        $_SESSION["tax"] = "0";
+        $_SESSION["total"] = "0";
     }
 }
