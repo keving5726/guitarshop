775eefeb231d44532168f27313386faafac20023
Kevin González <keving5726@gmail.com>
2025-04-14 18:59:46 -0400

    Add support for SQLite

--- config/database.php
+++ config/database.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 return
 [
     'driver' => getenv('DB_DRIVER') ?: 'mysql',
+    'path' => getenv('DB_PATH') ?: '',
     'host' => getenv('DB_HOST') ?: '127.0.0.1',
     'port' => (int) getenv('DB_PORT') ?: 3306,
     'database' => getenv('DB_NAME') ?: '',
